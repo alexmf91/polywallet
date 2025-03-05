00c7d3888aa8ce341605b6ef10710b1c679230be
alexmf91 <alexmunoz91@gmail.com>
2025-03-06 00:00:18 +0100

    feat: add AuthGuard component for route protection based on authentication

---
+++ frontend/src/components/guards/AuthGuard.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useAuth } from '@app/contexts'
+import { PrivateRoutes, PublicRoutes } from '@app/lib/routes'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+interface GuardProps {
+	children: React.ReactNode
+}
+
+const isRoutePrivate = (pathname: string): boolean => {
+	const privatePaths: string[] = Object.values(PrivateRoutes)
+	return privatePaths.includes(pathname)
+}
+
+export default function AuthGuard({ children }: GuardProps) {
+	const { isAuthenticated } = useAuth()
+	const router = useRouter()
+	const pathname = usePathname()
+
+	useEffect(() => {
+		if (!isAuthenticated && isRoutePrivate(pathname)) {
+			router.push(PublicRoutes.HOME)
+		}
+	}, [isAuthenticated, pathname, router])
+
+	return <>{children}</>
+}
