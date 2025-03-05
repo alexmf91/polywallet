822c07b108e110991478b000d62116420926a501
alexmf91 <alexmunoz91@gmail.com>
2025-03-05 11:49:47 +0100

    feat: add initial Prisma migration and schema for User model

---
+++ backend/prisma/migrations/20250305103945_initial_migration/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "walletAddress" TEXT NOT NULL,
+    "username" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_walletAddress_key" ON "User"("walletAddress");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
