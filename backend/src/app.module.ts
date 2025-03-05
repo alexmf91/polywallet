5f7a54cf16b898987fd6c1f0fe5534793b5fa178
alexmf91 <alexmunoz91@gmail.com>
2025-03-05 11:53:55 +0100

    feat: integrate middleware and configure app module with global config and modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { LoggerMiddleware } from './common/middleware'
+
+import { AuthModule } from './modules/auth'
+import { UserModule } from './modules/user'
+import { WalletModule } from './modules/wallet'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		AuthModule,
+		UserModule,
+		WalletModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes('')
+	}
+}
