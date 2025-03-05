835c0ded7f500ca7de468a12cb75332cbda8d08a
alexmf91 <alexmunoz91@gmail.com>
2025-03-05 23:58:50 +0100

    feat: add UI components

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,13 @@
+export * from './alert'
+export * from './button'
+export * from './card'
+export * from './command'
+export * from './dialog'
+export * from './dropdown-menu'
+export * from './input'
+export * from './label'
+export * from './popover'
+export * from './table'
+export * from './tabs'
+
+export { default as ModeToggle } from './mode-toggle'
