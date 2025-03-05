c19b860a9d81495bec28fa11024a9e66c89d6bc4
alexmf91 <alexmunoz91@gmail.com>
2025-03-05 11:53:46 +0100

    refactor: format app files

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,25 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 describe('AppController', () => {
-  let appController: AppController;
+	let appController: AppController
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+	beforeEach(async () => {
+		const app: TestingModule = await Test.createTestingModule({
+			controllers: [AppController],
+			providers: [AppService]
+		}).compile()
 
-    appController = app.get<AppController>(AppController);
-  });
+		appController = app.get<AppController>(AppController)
+	})
 
-  describe('root', () => {
-    it('returns "server is alive" message with "200" statusCode', () => {
-      expect(appController.getHealthCheck()).toEqual({
-        statusCode: 200,
-        message: 'server is alive',
-      });
-    });
-  });
-});
+	describe('root', () => {
+		it('returns "server is alive" message with "200" statusCode', () => {
+			expect(appController.getHealthCheck()).toEqual({
+				statusCode: 200,
+				message: 'server is alive'
+			})
+		})
+	})
+})
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHealthCheck() {
-    return { statusCode: 200, message: 'server is alive' };
-  }
+	getHealthCheck() {
+		return { statusCode: 200, message: 'server is alive' }
+	}
 }
