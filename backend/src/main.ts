a0b8e8e9e306c677abfd35329c3ebcd1cd7e7606
alexmf91 <alexmunoz91@gmail.com>
2025-03-05 11:49:36 +0100

    feat: enhance application bootstrap with global settings and error handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,38 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { HttpAdapterHost, NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
+
+import type { EnvironmentVariables } from 'env'
+import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './common/exception-filters'
+import { setupSwagger } from './config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	try {
+		const app = await NestFactory.create<NestExpressApplication>(AppModule)
+		const configService = app.get<ConfigService<EnvironmentVariables>>(ConfigService)
+
+		const PORT = configService.get('PORT', { infer: true }) || 3000
+		const VERSION = configService.get('VERSION', { infer: true })
+
+		const httpAdapterHost = app.get(HttpAdapterHost)
+
+		app.setGlobalPrefix(`api/v${VERSION}`)
+		app.enableCors()
+		app.useGlobalPipes(new ValidationPipe({ transform: true }))
+		app.useGlobalFilters(new HttpExceptionFilter(httpAdapterHost))
+
+		setupSwagger(app, configService)
+
+		await app.listen(PORT)
+		const url = await app.getUrl()
+
+		Logger.verbose(`Application is running on: ${url} ✔️`)
+		Logger.verbose(`Api documentation run on: ${url}/api-docs ✔️`)
+	} catch (error) {
+		Logger.error(`❌❌❌ ${error.message} ❌❌❌`)
+	}
 }
-bootstrap();
+
+bootstrap()
